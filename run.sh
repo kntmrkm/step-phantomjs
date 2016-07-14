8edfaa78e93e54936379b665e4e2c991384ac7ac
kntmrkm <kntmrkm@gmail.com>
2016-07-14 10:59:16 +0900

    change download link

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /tmp
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+wget https://www.dropbox.com/s/chkxd96vtg61eot/phantomjs-2.1.1-linux-x86_64.tar.bz2?dl=0
 tar xfj phantomjs-2.1.1-linux-x86_64.tar.bz2
 sudo cp /tmp/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin
